24c000d4752d7bfb783f8715ecaae1047fd64ec0
10956155 <dhruv.sgit@gmail.com>
2022-12-12 08:29:43 +0530

    Display current date and time challenge.

--- reactproject/src/index.js
+++ reactproject/src/index.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import ReactDOM from 'react-dom';
+const fname = 'Dhruv';
+const lname = 'Gupta';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const date = new Date();
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<>
+                  <h1>My name is {fname} {lname}</h1>
+                  <h1>Current Time is <b>{date.toLocaleTimeString()}</b></h1>
+                  <h1>Current Date is <b>{date.toLocaleDateString()}</b></h1>
+                </>, document.getElementById('root'));
+
+// ReactDOM.render(<>
+//                      <h1>{`My name is ${fname} ${lname}`}</h1>
+//                 </>, document.getElementById('root'));
+
+
+// ReactDOM.render(<>
+//                     <h1>Thapa Technical</h1> 
+//                     <p>Please like and share!!!</p> 
+//                     <h2> Subscribe my channel!!!</h2>
+//                 </>, document.getElementById('root'));
+
+
+
+// ReactDOM.render([<h1>Thapa Technical</h1>, 
+//                  <p>Please like and share!!!</p>,
+//                  <h2> Subscribe my channel!!!</h2>], 
+//                 document.getElementById('root'));
+
+
+//ReactDOM.render(<h1>Thapa Technical</h1> <p>Please like and share!!!</p>, document.getElementById('root'));
+
+
+
+
