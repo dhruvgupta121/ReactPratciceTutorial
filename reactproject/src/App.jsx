f86ea4374d6e29f4cefc3e29cc5aedb7275bb438
10956155 <dhruv.sgit@gmail.com>
2022-12-19 19:31:47 +0530

    Video 21.. Simple calculator app in ReactJS..

--- reactproject/src/App.jsx
+++ reactproject/src/App.jsx
@@ -1,25 +1,16 @@
 import React from "react";
-
-let greetingsMessage = "Good Morning";
-let currentTimeComponent = new Date().getHours();
-
-const styleSheet = {};
-
-if(currentTimeComponent >= 12 && currentTimeComponent < 17){
-    greetingsMessage = "Good Afternoon";
-    styleSheet.color = 'orange';
-}
-else if(currentTimeComponent >= 17 && currentTimeComponent < 20){
-    greetingsMessage = "Good Evening";
-    styleSheet.color = 'green';
-}
-else if(currentTimeComponent >= 20 && currentTimeComponent < 24){
-    greetingsMessage = "Good Night";
-    styleSheet.color = 'yellow';
-}
+import {add, sub, mult, div} from "./Calc.jsx";
 
 function App(){
-    return <h1 style={styleSheet}>Hello World from App Component... {greetingsMessage}</h1>;
-}
+
+   return (<>
+    <h1>Hello World!</h1>
+    <ol>
+       <li>Addition of two number is {add(30, 3)}</li>
+       <li>Subtraction of two number is {sub(30, 3)}</li>
+       <li>Multiplication of two number is {mult(30, 3)}</li>
+       <li>Divison of two number is {div(10, 3)}</li>
+    </ol>
+</>); }
 
 export default App;
